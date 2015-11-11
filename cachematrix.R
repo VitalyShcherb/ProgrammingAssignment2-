485dbf0168602e1e9a92868840d090d3ad6cf849
VitalyShcherb <me5k@mail.ru>
2015-11-11 16:11:35 +0300

    Update and rename .gitignore to cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,25 @@
+makeCacheMatrix <- function(x = matrix()) {
+    Inv <- NULL 
+    set <- function(y) {
+        x <<- y
+        Inv <<- NULL 
+    }
+    get <- function() x
+    setInv <- function(inv) Inv <<- inv 
+    getInv <- function() Inv 
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
+}
+
+cacheSolve <- function(x, ...) {
+    Inv <- x$getInv() 
+    if(!is.null(Inv)) { 
+        message("getting cached data")
+        return(Inv) 
+    }
+    data <- x$get() 
+    Inv <- solve(data) 
+    x$setInv(Inv) 
+    Inv
+}
